bd233c49edf3df9a4040ce501a326732c2242ec0
Aaron Krenning <aaronkrenning@outlook.com>
2020-10-09 15:53:58 -0500

    Add bulk of project, configuring form reset

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const button = document.querySelector("button");
+
+const empName = document.querySelector("#empName");
+const details = document.querySelector("#complaint");
+
+//TODO: Listen for a 'click' on that button
+button.addEventListener("click", function () {
+  console.log("Form Submitted");
+});
+//TODO: When there is a click, change the p tag to say '...'
+
+empName.addEventListener("focus", () => {
+  console.log("Entered input");
+});
+
+empName.addEventListener("blur", () => {
+  console.log("Left input");
+});
+
+details.addEventListener("keydown", (e) => {
+  console.log("currently typing in: '${e.target}'");
+});
+
+//Query Selector
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Prevented default from occurring", e);
+
+  var inputs = document.getElementsByTagName("input"),
+    clearTypes = { text: 1, details: 1 }, // etc. you fill this in
+    i = inputs.length,
+    input;
+
+  while (i) {
+    if (clearTypes[(input = inputs[--i]).type]) {
+      input.value = "";
+    }
+  }
+});
