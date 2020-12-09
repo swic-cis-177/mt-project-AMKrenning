0b12f65665e08baa997565aff79359694adba0c2
Aaron Krenning <aaronkrenning@outlook.com>
2020-12-09 11:31:23 -0600

    Tried to find a way to make form create table from objects in the array

--- lib.js
+++ lib.js
@@ -7,6 +7,7 @@ const tbody = document.querySelector("tbody");
  */
 export const processForm = (elements) =>
   // Create an actual Array for 'superpowers' 🦸🏾‍♂️
+
   Array.from(elements)
 
     // Does it have an id attribute?
@@ -27,15 +28,12 @@ export const processForm = (elements) =>
       // Initialize form data as an empty object
       {}
     );
-
-export const createTable = (elements) => {
-  elements.forEach(({ empName, dept, mgr }) => {
-    const newComplaintRow = template.content.cloneNode(true);
-    const newComplaintTDs = newComplaintRow.querySelectorAll("td");
-
-    newComplaintTDs[0].textContent = empName;
-    newComplaintTDs[1].textContent = dept;
-    newComplaintTDs[2].textContent = mgr;
-    tbody.appendChild(newComplaintRow);
-  });
+/**
+ * Filters out all of the 'id input's and reduces to an object using ids/values
+ * @param {HTMLFormControlsCollection} elements
+ * @returns {Object}
+ */
+export const addComplaint = (elements) => {
+  processForm(elements);
+  Array.push(elements);
 };
