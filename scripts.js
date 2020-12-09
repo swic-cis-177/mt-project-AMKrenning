a0600dcd902a86f5d285ede685a40f8b7e72f846
Aaron Krenning <aaronkrenning@outlook.com>
2020-12-09 10:06:59 -0600

    Remove extraneous listeners and selectors, fixed submit event listener

--- scripts.js
+++ scripts.js
@@ -1,34 +1,10 @@
 import processForm from "./lib.js";
 
-const button = document.querySelector("button");
-const empName = document.querySelector("#empName");
-const details = document.querySelector("#complaint");
-const inputs = document.querySelectorAll("input");
 const form = document.querySelector("form");
 
 //Query Selector
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-});
 
-button.addEventListener("click", (elements) => {
-  console.log("Form Submitted");
-  inputs.forEach((input) => (input.value = ""));
-  let complaint = {
-    empName: document.getElementById("empName").value,
-    details: document.getElementById("complaint").value,
-  };
-  processForm(complaint);
-});
-
-empName.addEventListener("focus", () => {
-  console.log("Entered input");
-});
-
-empName.addEventListener("blur", () => {
-  console.log("Left input");
-});
-
-details.addEventListener("keydown", () => {
-  console.log("currently typing in: '${e.target}'");
+  console.log(processForm(e.target.elements));
 });
